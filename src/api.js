943913823d36eef5e7d89209a2d8dbb5680e699f
fouad mesrane <fouadmesrane@gmail.com>
2025-01-14 20:44:23 -0700

    added a fetch function to get data from harry potter api

--- src/api.js
+++ src/api.js
@@ -1,13 +1,14 @@
 
-
 const BASE_URL = "https://potterapi-fedeperin.vercel.app/en";
-
 export const fetchData = async (param) => {
-  try {
-    const data = await axios.get(`${BASE_URL}/${param}`);
-    const returnedData = data.data
-    return  returnedData;
-  } catch (error) {
-    console.error(`Error fetching ${param}`, error);
-  }
-};
+    try {
+      const data = await axios.get(`${BASE_URL}/${param}`);
+      const returnedData = data.data
+      return  returnedData;
+    } catch (error) {
+      console.error(`Error fetching ${param}`, error);
+    }
+  };
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,17 +1,68 @@
-// import  {fetchData}  from "./api.js";
 
-// const getData = async (param) => {
-//   try {
-//     const data = await fetchData(param)
-//     return data
-//   } catch {
-//     console.error("Failed getting data", error);
+import { fetchData } from "./api.js";
+// api url
+
+
+//select
+const heroWrapper = document.querySelector('#hero-wrapper')
+const selectEl = document.getElementById('select-option')
+const searchBtn = document.getElementById('search-btn')
+const booksEl = document.getElementById('books')
+const charEl = document.getElementById('characters')
+const housesEl = document.getElementById('houses')
+const spellsEl = document.getElementById('spells')
+
+searchBtn.addEventListener('click', async (e) => {
+   try {
+    const selectValue = selectEl.value.toString()
+    if(selectValue === 'books') {
+     const books = await fetchData(selectValue)
+     books.map (book => {
+        const div = document.createElement('div')
+        const img = document.createElement('img')
+        console.log(book)
+        // div.classList.add()
+        img.src = book.cover
+        img.style.width = "200px",
+
+
+        div.appendChild(img)
+        booksEl.appendChild(div)
+     })   
+
+
+     heroWrapper.classList.add('hidden')
     
-//   }
-// };
+     
 
-//  const books = await getData('books')
-//  books.map(book => {
-//     console.log(book)
-//  })
+    } else if (selectValue === 'characters') {
+        
+    } else if (selectValue === 'houses') {
+
+    }  else if (selectValue === 'spells') {
+        
+    }
+   } catch (error) {
+    console.error(`Error fetching data` , error)
+   }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
