87bb62998cab2215a90abb2d4e31974026006702
fouad mesrane <fouadmesrane@gmail.com>
2025-01-15 09:40:05 -0700

    rendered title and description from the api

--- src/main.js
+++ src/main.js
@@ -15,19 +15,26 @@ searchBtn.addEventListener("click", async (e) => {
   try {
     const selectValue = selectEl.value.toString();
     if (selectValue === "books") {
-        // fetching the books from api
+      // fetching the books from api
       const books = await fetchData(selectValue);
       books.map((book) => {
         //creating elements to render the books
         const div = document.createElement("div");
         const img = document.createElement("img");
         const btn = document.createElement("button");
-        
-        btn.textContent = "details"
-        btn.classList.add("bg-slate-800", "px-4", "py-2" ,"mt-4","rounded-md", "text-white")               
+
+        btn.textContent = "details";
+        btn.classList.add(
+          "bg-slate-800",
+          "px-4",
+          "py-2",
+          "mt-4",
+          "rounded-md",
+          "text-white"
+        );
         img.src = book.cover;
         div.style.width = "300px";
-        div.classList.add("text-center")
+        div.classList.add("text-center");
         img.style.width = "100%";
 
         div.appendChild(img);
@@ -35,10 +42,28 @@ searchBtn.addEventListener("click", async (e) => {
         booksEl.appendChild(div);
 
         // showing more details when clicking on details button
-        btn.addEventListener('click', (e) => {
-            e.stopPropagation()
-            img.src = ''
-        })
+        const title = document.createElement("h2");
+        const description = document.createElement('p')
+        title.classList.add("text-orange-300", "font-bold")
+        description.classList.add('text-xs', 'text-white','font-normal')
+
+        btn.addEventListener("click", (e) => {
+
+          if (e.target.textContent === "details") {
+            img.src = "";
+            btn.textContent = "back";
+            title.textContent = book.title;
+            description.textContent = book.description
+            div.insertBefore(title, div.firstChild);
+            div.insertBefore(description, div.lastChild);
+          } else {
+            img.src = book.cover;
+            title.textContent = ""
+            btn.textContent = "details";
+
+          
+          }
+        });
       });
 
       heroWrapper.classList.add("hidden");
