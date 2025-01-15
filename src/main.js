3af59cb651d0a1e05b857725fde997ad2d27a5cf
fouad mesrane <fouadmesrane@gmail.com>
2025-01-14 22:28:22 -0700

    added details button to the books section

--- src/main.js
+++ src/main.js
@@ -1,68 +1,64 @@
-
 import { fetchData } from "./api.js";
 // api url
 
-
 //select
-const heroWrapper = document.querySelector('#hero-wrapper')
-const selectEl = document.getElementById('select-option')
-const searchBtn = document.getElementById('search-btn')
-const booksEl = document.getElementById('books')
-const charEl = document.getElementById('characters')
-const housesEl = document.getElementById('houses')
-const spellsEl = document.getElementById('spells')
+const homeBtn = document.getElementById("home-btn");
+const heroWrapper = document.querySelector("#hero-wrapper");
+const selectEl = document.getElementById("select-option");
+const searchBtn = document.getElementById("search-btn");
+const booksEl = document.getElementById("books");
+const charEl = document.getElementById("characters");
+const housesEl = document.getElementById("houses");
+const spellsEl = document.getElementById("spells");
 
-searchBtn.addEventListener('click', async (e) => {
-   try {
-    const selectValue = selectEl.value.toString()
-    if(selectValue === 'books') {
-     const books = await fetchData(selectValue)
-     books.map (book => {
-        const div = document.createElement('div')
-        const img = document.createElement('img')
-        console.log(book)
-        // div.classList.add()
-        img.src = book.cover
-        img.style.width = "200px",
-
-
-        div.appendChild(img)
-        booksEl.appendChild(div)
-     })   
-
-
-     heroWrapper.classList.add('hidden')
-    
-     
-
-    } else if (selectValue === 'characters') {
+searchBtn.addEventListener("click", async (e) => {
+  try {
+    const selectValue = selectEl.value.toString();
+    if (selectValue === "books") {
+        // fetching the books from api
+      const books = await fetchData(selectValue);
+      books.map((book) => {
+        //creating elements to render the books
+        const div = document.createElement("div");
+        const img = document.createElement("img");
+        const btn = document.createElement("button");
         
-    } else if (selectValue === 'houses') {
+        btn.textContent = "details"
+        btn.classList.add("bg-slate-800", "px-4", "py-2" ,"mt-4","rounded-md", "text-white")               
+        img.src = book.cover;
+        div.style.width = "300px";
+        div.classList.add("text-center")
+        img.style.width = "100%";
 
-    }  else if (selectValue === 'spells') {
-        
+        div.appendChild(img);
+        div.appendChild(btn);
+        booksEl.appendChild(div);
+
+        // showing more details when clicking on details button
+        btn.addEventListener('click', (e) => {
+            e.stopPropagation()
+            img.src = ''
+        })
+      });
+
+      heroWrapper.classList.add("hidden");
+    } else if (selectValue === "characters") {
+    } else if (selectValue === "houses") {
+    } else if (selectValue === "spells") {
     }
-   } catch (error) {
-    console.error(`Error fetching data` , error)
-   }
-})
+  } catch (error) {
+    console.error(`Error fetching data`, error);
+  }
+});
 
+// home page reset
+function homePage() {
+  heroWrapper.classList.remove("hidden");
+  selectEl.value = selectEl[0].textContent;
+  booksEl.textContent = "";
+  charEl.textContent = "";
+  housesEl.textContent = "";
+  spellsEl.textContent = "";
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+homeBtn.addEventListener("click", homePage);
