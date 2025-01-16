e01f12c531d1b82c2bd2b9d36ec5e383e3ba5ce8
fouad mesrane <fouadmesrane@gmail.com>
2025-01-16 11:03:18 -0700

    added characters link functionality

--- src/characters.js
+++ src/characters.js
@@ -1,13 +1,13 @@
 import { fetchData } from "./api.js";
-import { selectEl, charEl, heroWrapper, booksEl, spellsEl } from "./elements.js";
+import { selectEl, charEl, heroWrapper, booksEl, spellsEl, charLink } from "./elements.js";
 
 export default async function renderCharcters() {
   try {
     const selectValue = selectEl.value.toLowerCase();
-
-    if (selectValue === "characters") {
+    const charValue = charLink.textContent.trim().toLowerCase()
+    if (selectValue === "characters" || charValue === "characters") {
       // fetching the characters from api
-      const characters = await fetchData(selectValue);
+      const characters = await fetchData("characters");
       characters.map((char) => {
         //creating elements to render the characters
         const div = document.createElement("div");
